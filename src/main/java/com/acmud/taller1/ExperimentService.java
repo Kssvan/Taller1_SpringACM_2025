8a1c4db5243a294de40f2218b2e1f7f1acdfb7c3
Karen Sanchez <kssanchezv@udistrital.edu.co>
2025-10-18 19:44:05 -0500

    punto1, reto1

---
+++ src/main/java/com/acmud/taller1/ExperimentService.java
@@ -0,0 +1,14 @@
+package com.acmud.taller1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Component("ExperimentService")
+public class ExperimentService {
+
+	public ExperimentService() {
+        System.out.println("ExperimentService")
+
+    }
+
+}
